dabc7f03870b7c210cde41e2138ffb9cb84e6695
Dilyan Grigorov <dido.grigorov@gmail.com>
2024-08-16 18:57:24 +0300

    Update install_sqlite.sh

--- install_sqlite.sh
+++ install_sqlite.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Download and install SQLite 3.41.0 (or the version you need)
-wget https://www.sqlite.org/2023/sqlite-autoconf-3410000.tar.gz
-tar -xvf sqlite-autoconf-3410000.tar.gz
-cd sqlite-autoconf-3410000
+wget https://www.sqlite.org/2023/sqlite-autoconf-3460100.tar.gz
+tar -xvf sqlite-autoconf-3460100.tar.gz
+cd sqlite-autoconf-3460100
 
 ./configure --prefix=/usr/local
 make
 make install
-
-# Verify installation
-sqlite3 --version
