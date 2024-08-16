690a590010b395a51e50a8a24c12be5087491b34
Dilyan Grigorov <dido.grigorov@gmail.com>
2024-08-16 19:11:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+# Download and install SQLite 3.41.0 (or the version you need)
+wget https://www.sqlite.org/2024/sqlite-autoconf-3460100.tar.gz
+tar -xvf sqlite-autoconf-3460100.tar.gz
+cd sqlite-autoconf-3460100
+
+./configure --prefix=/usr/local
+make
+make install
+
 # Exit on error
 set -o errexit
 
