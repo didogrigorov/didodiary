58795830ffd341d9733e6194859481bdf069023f
Dilyan Grigorov <dido.grigorov@gmail.com>
2024-08-18 14:51:01 +0300

    Update settings.py

--- didodiary/settings.py
+++ didodiary/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
