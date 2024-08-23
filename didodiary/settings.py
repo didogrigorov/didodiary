6da34040ba3684709430904ad26669095a00106f
Dilyan Grigorov <dido.grigorov@gmail.com>
2024-08-23 19:10:52 +0300

    Update settings.py

--- didodiary/settings.py
+++ didodiary/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
